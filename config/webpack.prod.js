8f52148926e3b3b97cb426db4290be168063eadd
jmauwerb <jeroen.werbrouck@ugent.be>
2021-09-15 17:49:04 +0200

    domain test error

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: "[name].[contenthash].js",
-        publicPath: `${packageJSON.name}/lfatest/`
+        publicPath: `${packageJSON.name}/latest/`
     },
     plugins: [
         new ModuleFederationPlugin({
