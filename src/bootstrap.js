fc1ae3c6687c9b69d395c612acb2e029928c43ca
jmauwerb <jeroen.werbrouck@ugent.be>
2021-09-15 17:37:47 +0200

    domain test

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,14 +6,12 @@ const mount = (el) => {
   ReactDOM.render(<App/>, el);
 };
 
-// if (process.env.NODE_ENV === "development") {
-  // if there is a div with the unique id (see public/index.html)
-  // there cannot be a div element with this id anywhere else! (therefore we're trying to make that as unique as possible)
-  const el = document.querySelector("#a4fead9a-19b3-4663-aecd-b7854874ea34");
-  if (el) {
-    // we are running the project in isolation
-    mount(el);
-  }
-// }
+// if there is a div with the unique id (see public/index.html)
+// there cannot be a div element with this id anywhere else! (therefore we're trying to make that as unique as possible)
+const el = document.querySelector("#a4fead9a-19b3-4663-aecd-b7854874ea34");
+if (el) {
+  // we are running the project in isolation
+  mount(el);
+}
 
 export { mount };
