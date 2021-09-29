7aabd828586eddda2a2f4a7dd4b4c0cb67f4f459
jmauwerb <jeroen.werbrouck@ugent.be>
2021-09-29 11:18:22 +0200

    action to gh pages test

--- src/isolatedComponents/SignIn.js
+++ src/isolatedComponents/SignIn.js
@@ -56,7 +56,7 @@ export default function SignIn() {
     if (!session.info.isLoggedIn) {
       await login({
         oidcIssuer,
-        redirectUrl: window.location.origin + "/",
+        redirectUrl: window.location.href + "/",
         clientName: "lbdserver",
       });
     }
