ef1f7d9e79d3a35e2214e56c33437cfed82b15aa
jmauwerb <jeroen.werbrouck@ugent.be>
2021-09-30 16:01:38 +0200

    shared dependencies singleton

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,7 +2,7 @@ const {merge} = require('webpack-merge')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJSON = require('../package.json')
-
+const deps = packageJSON.dependencies
 const prodConfig = {
     mode: 'production',
     output: {
